87711cd080c550af2674b266bc5a4a52ce4876ab
panntod <munjalindra.pandhu@gmail.com>
2025-01-31 09:48:07 +0700

    refactor: Update Vite config to use path.resolve for alias

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
   },
-  server: { port: 3000 },
-  preview: { port: 3000 },
-  plugins: [react()],
+  plugins: [react()]
 });
