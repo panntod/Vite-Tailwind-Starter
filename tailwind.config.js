9ccd30fb4bb9e65ec9bd69950a01387c72d467f5
panntod <munjalindra.pandhu@gmail.com>
2025-01-31 09:50:34 +0700

    chore: Clean up configuration files and ensure consistent formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/components/**/*.{jsx,tsx}',
-    './src/pages/**/*.{jsx,tsx}',
-    './src/**/*.{jsx,tsx}',
-    './src/index.html'
+    "./src/components/**/*.{jsx,tsx}",
+    "./src/pages/**/*.{jsx,tsx}",
+    "./src/**/*.{jsx,tsx}",
+    "./src/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 };
